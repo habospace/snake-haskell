52029d60ecaf8cbdf891de2d313ae824387f72ef
Janos Matyas Boros <mboros@fxcintel.com>
2019-01-30 01:29:11 +0000

    1. derive functor, applicative and monad instance for wrapper type. 2. add gamestate updater functions.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,17 @@
 
+data Wrap a = Wrap a deriving (Eq, Show)
+
+instance Functor Wrap where
+  fmap f (Wrap x) = Wrap (f x)
+
+instance Applicative Wrap where
+  pure = Wrap
+  (<*>) (Wrap f) (Wrap x) = Wrap (f x)
+
+instance Monad Wrap where
+  return = pure
+  (>>=) (Wrap x) f = f x
+
 data Direction =
     Up
   | Down
@@ -15,7 +28,9 @@ data GameState = GameState {
     height :: Int,
     snake :: Snake,
     score :: Int,
-    food :: (Int, Int)
+    food :: (Int, Int),
+    justAte :: Bool,
+    inGame :: Bool
   } deriving (Eq, Show)
 
 mapDirection :: Direction -> (Int, Int)
@@ -51,6 +66,9 @@ checkBodyCollision s = overlap (fst nextHeadPos, snd nextHeadPos) (body s) where
     | h == b = True
     | otherwise = overlap h bs
 
+checkFoodConsumption :: GameState -> Bool
+checkFoodConsumption gs = (food gs) == (head . body . snake $ gs)
+
 moveSnake :: Snake -> Snake
 moveSnake s = Snake (nextHeadPos:(newTail . body $ s)) (direction s) where
   nextHeadPos = getNextSnakeHeadPos s
@@ -61,4 +79,26 @@ moveSnake s = Snake (nextHeadPos:(newTail . body $ s)) (direction s) where
 consumeFood :: Snake -> Snake
 consumeFood s = Snake (nextHeadPos:(body s)) (direction s) where
   nextHeadPos = getNextSnakeHeadPos s
+
+--updateGameAfterMoving :: GameState -> GameState
+--updateGameAfterMoving gs =
+--    GameState (width gs) (height gs) (consumeFood . snake $ gs)
+--              ((+1) . score $ gs) (width gs, height gs) True True
+
+updateGameAfterEating :: GameState -> GameState
+updateGameAfterEating gs =
+  GameState (width gs) (height gs) (consumeFood . snake $ gs)
+            ((+1) . score $ gs) (width gs, height gs) True True
+
+setInGameState :: GameState -> Bool -> GameState
+setInGameState gs igs =
+  GameState (width gs) (height gs) (snake gs)
+            (score gs) (food gs) (justAte gs) igs
+
+{-tickGame :: GameState -> Wrap GameState
+tickGame gs = do
+  gs1 <- (\x -> if (checkWallCollision x) || (checkBodyCollision . snake $ x) then return (setInGameState x False) else return x) gs
+  gs2 <- (\x -> if ((inGame x) /= False) && (checkFoodConsumption x) then return (updateGameAfterEating x) else return x) gs1
+  (\x -> if (inGame x) == True then return (moveSnake x) else return x) gs2
+-}
 
